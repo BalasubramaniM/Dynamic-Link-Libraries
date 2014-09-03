59d6b3b2e981dc4a369f283985714cf7b3ebe373
Balasubramani <balageetha@live.com>
2014-09-03 17:40:10 +0530

    HelperClass.dll Class file contains some important Code Snippets
    BaseClass.cs and Serialization.cs helps you to use functions in HelperClass.dll and effective usage of it.

---
+++ BaseClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using HelperClass.Base; // Namespace to make use of BasePage class
+
+namespace HelperClass.Base
+{
+    public class BaseClass
+    {
+        public void HelperFunction()
+        {
+            BasePage.Instance.NavigateToPage("MainPage.xaml"); // Pass the name of the page as String to navigate to that page
+
+            string result = BasePage.Instance.ReadFile("TextFileName.txt"); // Pass the name of the text file to read data
+
+            // Validation of Number - text box.
+            // You can enter only numbers from 1 - 9 and back key in this validation.
+            // Cannot insert special characters like . even when Input Scope in Number.
+            TextBox.KeyDown += new KeyEventHandler(BasePage.Instance.NumberValidation);
+
+            // Validation of Text - text box.
+            // You can enter only alphabets from a - z, space & back key in this validation.
+            // Cannot insert special characters like ., $ anything apart from that.
+            TextBox.KeyDown += new KeyEventHandler(BasePage.Instance.TextValidation);
+
+            BasePage.Instance.NotifyPropertyChanged("PropertyName"); // Just pass the name of the Property to notify change in property.
+
+            // You can save the string value and retrieve back anywhere in your project in single session
+            BasePage.Instance.SaveStringValue("name", "BalasubramaniM"); // String "BalasubramaniM" is saved in key value called "name"
+
+            string name = BasePage.Instance.GetStringValue("name"); // retrieve the value by passing its key value
+
+            BasePage.Instance.MessageBoxPrint("Text to be printed in Message Box"); // Just pass the string to get printed in MessageBox
+
+            BasePage.Instance.DebugPrint("Text to be printed in Console(debug) window"); // Pass the string to get displayed in the console(debug or output) window.
+        }
+    }
+}
--- Serialization.cs
+++ Serialization.cs
@@ -3,31 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Newtonsoft.Json.Linq;
+using System.Windows;
+using System.IO;
 using HelperClass.Serialization; // Namespace to make use of Serialization class
 
-namespace Controls.Model
+namespace HelperClass.Serialization
 {
     class Serialization
     {
         protected object DeserializeFromJson(object obj, string jsonString) // Converts JSON string to objects
         {
-            return Serializer.Serialize.DeserializeFromJson(obj, jsonString);
+            return Serializer.Instance.DeserializeFromJson(obj, jsonString);
         }
 
         protected string SerializeToJson(object obj) // Converts objects to JSON string (in JSON format)
         {
-            return Serializer.Serialize.SerializeToJson(obj);
+            return Serializer.Instance.SerializeToJson(obj);
         }
 
         protected object DeserializeFromXML(object obj, string xmlString) // Converts XML data to objects
         {
-            return Serializer.Serialize.DeserializeFromXML(obj, xmlString);
+            return Serializer.Instance.DeserializeFromXML(obj, xmlString);
         }
 
         protected string SerializeToXML(object obj) // Converts objects to XML value (in XML format)
         {
-            return Serializer.Serialize.SerializeToXML(obj);
+            return Serializer.Instance.SerializeToXML(obj);
+        }
+
+        public JObject JsonData { get; set; }
+        protected void JsonValue()
+        {
+            JsonData = Serializer.Instance.RetrieveParsedJSONValue("JsonFileName.txt"); // Get parsed json data, when you pass json string to this function
         }
     }
 }
