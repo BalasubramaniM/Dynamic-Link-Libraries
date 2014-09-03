f0b2a87b5400a8180b3c1a06911d13673a22e76a
Balasubramani <balageetha@live.com>
2014-09-03 14:41:20 +0530

    This class helps you to use Serialization.dll file and its methods to convert JSON and XML strings to objects and vice versa.

---
+++ Serialization.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HelperClass.Serialization; // Namespace to make use of Serialization class
+
+namespace Controls.Model
+{
+    class Serialization
+    {
+        protected object DeserializeFromJson(object obj, string jsonString) // Converts JSON string to objects
+        {
+            return Serializer.Serialize.DeserializeFromJson(obj, jsonString);
+        }
+
+        protected string SerializeToJson(object obj) // Converts objects to JSON string (in JSON format)
+        {
+            return Serializer.Serialize.SerializeToJson(obj);
+        }
+
+        protected object DeserializeFromXML(object obj, string xmlString) // Converts XML data to objects
+        {
+            return Serializer.Serialize.DeserializeFromXML(obj, xmlString);
+        }
+
+        protected string SerializeToXML(object obj) // Converts objects to XML value (in XML format)
+        {
+            return Serializer.Serialize.SerializeToXML(obj);
+        }
+    }
+}
